9b0abf39eb111dc973b1a3b224c4b61784f39326
nahalhz <113655682+nahalhz@users.noreply.github.com>
2022-09-18 21:51:49 -0400

    Update FloodIt Nahal.py

--- FloodIt Nahal.py
+++ FloodIt Nahal.py
@@ -1,14 +1,7 @@
 #########################################################################################################
 ## File Name: FloodIt-Nahal.py                                                                         ##
-## Author: Nahal H.                                                                                    ##
-## Date: June 22 2020                                                                                  ##
+## Author: Nahal H.                                                                                    ##                                                                               ##
 ## Modules Used: pygame, string, time, Controls                                                        ##
-## Description: This game uses the grid class from Controls module to create a game grid of colorful   ##
-## tiles with random colors, and using a recursive function of flood it, user has to try to fill the   ##
-## grid with one colour within a set number of moves that is calculated based on number of tiles and   ##
-##  number of colours used. Both these variables can be changed using comboboxes and the grid will be  ##
-## reset by a button. Winning and losing is declared to the users as well.                             ##
-## User Input: Inputs the size of game grid and the number of colours using the options in comboboxes. ##
 #########################################################################################################
 
 #----------------#
